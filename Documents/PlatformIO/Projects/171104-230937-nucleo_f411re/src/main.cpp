16c6bdbeff1c7e04e7337f2acfc26c931af11bb7
Mr Canival <31094110+tretap@users.noreply.github.com>
2017-11-06 00:57:56 +0700

    Update : Yaw >> Gyro , Delete roll & pitch

--- Documents/PlatformIO/Projects/171104-230937-nucleo_f411re/src/main.cpp
+++ Documents/PlatformIO/Projects/171104-230937-nucleo_f411re/src/main.cpp
@@ -2,54 +2,26 @@
 
 int main() {
 
-    // put your setup code here, to run once:
-    char roll_cmd;
 
-    float x,y,z;
-    int gyro_x,gyro_y,gyro_z,accel_x,accel_y,accel_z;
+    // put your setup code here, to run once:
+    pc.printf("Hello world\n");
+    float z,yaw;
+    int gyro_z;
 
     write_setting_byte(power_mgmt_1,0);
 
+    init_offset();
+    //pc.printf("%d",gyro_avr);
+
     while(1) {
-        // put your main code here, to run repeatedly:
-        //while(btn == 1);
-
-        //pc.printf("-----------Gyro------------\n");
-
-        //get_gyro(&gyro_x,&gyro_y,&gyro_z);
-
-        roll_cmd = 0x43;
-        read_word_2c(&roll_cmd, &gyro_x);
-        roll_cmd = 0x45;
-        read_word_2c(&roll_cmd, &gyro_y);
-        roll_cmd = 0x47;
-        read_word_2c(&roll_cmd, &gyro_z);
-
-        //pc.printf("Gyro X: %f \n",(float)gyro_x/131);
-        //pc.printf("Gyro Y: %f \n",(float)gyro_y/131);
-        //pc.printf("Gyro Z: %f \n",(float)gyro_z/131);
-
-        //pc.printf("-----------Accelo------------\n");
-
-        //get_accelometer(&accel_x,&accel_y,&accel_z);
-
-        roll_cmd = 0x3B;
-        read_word_2c(&roll_cmd, &accel_x);
-        roll_cmd = 0x3D;
-        read_word_2c(&roll_cmd, &accel_y);
-        roll_cmd = 0x3F;
-        read_word_2c(&roll_cmd, &accel_z);
-
-        //pc.printf("Accelo X: %f \n",(float)accel_x/16384.0);
-        //pc.printf("Accelo Y: %f \n",(float)accel_y/16384.0);
-        //pc.printf("Accelo Z: %f \n",(float)accel_z/16384.0);
-
-        x = get_pitch_rotation((float)accel_x/16384.0, (float)accel_y/16384.0, (float)accel_z/16384.0);
-        y = get_roll_rotation((float)accel_x/16384.0, (float)accel_y/16384.0, (float)accel_z/16384.0);
-        z = get_yaw_rotation((float)accel_x/16384.0, (float)accel_y/16384.0, (float)accel_z/16384.0);
-
-        pc.printf("X: %f, Y: %f, Z: %f \n",x,y,z);
-
+        t.start();
+        read_word(&roll_cmd, &gyro_z);
+        gyro_z = gyro_z - gyro_avr ;
+        z = ((gyro_z / 32767.0) * 250.0) * (PI/180);
+        t.stop();
+        yaw += -t.read() * z;
+        pc.printf(" Yaw: %f time : %f\n",z,t.read());
+        t.reset();
     }
 }
 
@@ -75,12 +47,12 @@ void read_word(char* cmd,int* data){
   read_byte(cmd,&high);
   (*cmd++);
   read_byte(cmd,&low);
-
   (*cmd--);
-  *data = (high << 8) + low;
+
+  *data = (int16_t)((high << 8) | low);
 }
 
-void read_word_2c(char*cmd, int*data){
+void read_word_2c(char*cmd, float*data){
   int val;
   read_word(cmd,&val);
 
@@ -91,46 +63,14 @@ void read_word_2c(char*cmd, int*data){
   }
 }
 
-void get_gyro(int* x, int* y, int* z){
-  char roll_cmd;
+void init_offset(){
+  float sum_gyro =0;
+  int data_now;
+  for(int i = 0; i<500;i++){
+    pc.printf("%d\n",i);
+    read_word(&roll_cmd, &data_now);
+    sum_gyro = sum_gyro +data_now;
+  }
 
-  roll_cmd = 0x43;
-  read_word_2c(&roll_cmd, x);
-  *x = *x / 131;
-  roll_cmd = 0x45;
-  read_word_2c(&roll_cmd, y);
-  *y = *y / 131;
-  roll_cmd = 0x47;
-  read_word_2c(&roll_cmd, z);
-  *z = *z / 131;
-}
-
-void get_accelometer(int* x, int* y, int* z){
-  char roll_cmd;
-
-  roll_cmd = 0x3B;
-  read_word_2c(&roll_cmd, x);
-  *x = *x / 16384.0;
-  roll_cmd = 0x3D;
-  read_word_2c(&roll_cmd, y);
-  *y = *y / 16384.0;
-  roll_cmd = 0x3F;
-  read_word_2c(&roll_cmd, z);
-  *z = *z /16384.0;
-}
-
-float dist(float a, float b){
-  return sqrt(a*a+b*b);
-}
-
-float get_pitch_rotation(float x, float y ,float z){
-  return -(atan2(x,dist(y,z)) * 180/PI);
-}
-
-float get_roll_rotation(float x, float y, float z){
-  return atan2(y, dist(x,z)) * 180/PI;
-}
-
-float get_yaw_rotation(float x, float y, float z){
-  return atan2(dist(y,z),z) * 180/PI;
+  gyro_avr = sum_gyro/500.0;
 }
